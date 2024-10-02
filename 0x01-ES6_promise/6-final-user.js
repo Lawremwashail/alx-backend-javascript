f4fa43ff3648c4c92469aea6805700a484807083
Lawremwashail <lawremwash@gmail.com>
2024-10-02 23:50:35 +0300

    final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+    const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)]
+    
+    return Promise.allSettled(promises)
+        .then((results) => {
+            return results.map((result) => ({
+                status: result.status,
+                value: result.status === 'fulfilled' ? result.value : result.reason,
+            }))
+        })
+}
