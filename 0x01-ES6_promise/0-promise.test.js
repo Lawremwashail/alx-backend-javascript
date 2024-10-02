da106dc8a23b64b3fba209754ac330c04242dc9f
Lawremwashail <lawremwash@gmail.com>
2024-10-02 20:04:21 +0300

    promise

---
+++ 0x01-ES6_promise/0-promise.test.js
@@ -0,0 +1,6 @@
+import getResponseFromAPI from './0-promise';
+
+test('getResponseFromAPI returns a Promise', () => {
+	  const response = getResponseFromAPI();
+	  expect(response instanceof Promise).toBe(true);
+});
