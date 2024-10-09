8c0bee77a34dce4f507571e3e4d2be073d97589e
Lawremwashail <lawremwash@gmail.com>
2024-10-09 19:15:28 +0300

    classstudent

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -29,3 +29,31 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentConstructor {
+    new (firstName: string,
+    lastName: string): Student;
+}
+
+interface Student {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName
+
+    }
+    workOnHomework(): string {
+        return "currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
