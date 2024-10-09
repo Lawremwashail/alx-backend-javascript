f9a2902b5b71cf8cf224008981347cad7a228181
Lawremwashail <lawremwash@gmail.com>
2024-10-09 18:38:54 +0300

    task_1

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,25 +1,17 @@
-class Teacher {
-    firstName: string;
-    lastName: string;
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
     
     [key: string]: any;
-
-
-    //constructor implementation
-    constructor(
-        firstName: string,
-        lastName: string,
-        fullTimeEmployee: boolean,
-        location: string,
-        yearsOfExperience?: number
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName
-        this.fullTimeEmployee = fullTimeEmployee;
-        this.location = location;
-        this.yearsOfExperience = yearsOfExperience;
-    }
 }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
